588927a40862284148fcef1299403369f5d0dd81
Geoff Reedy <geoff@programmer-monk.net>
2009-12-13 21:50:12 -0700

    Add tag kind extractor

--- src/main/scala/sctags/extractors.scala
+++ src/main/scala/sctags/extractors.scala
@@ -25,9 +25,17 @@ object extractors {
     case tree => _.search = tree.pos.lineContent
   }
 
+  val kind = extractor {
+    case _:Trees#ModuleDef => _.kind = "O"
+    case _:Trees#ClassDef => _.kind = "c"
+    case _:Trees#TypeDef => _.kind = "t"
+    case tree:Trees#ValDef => _.kind = if (tree.mods.isVariable) "v" else "V"
+    case _:Trees#DefDef => _.kind = "f"
+  }
+
   def sequence(exs: Seq[Extractor]): Extractor = {
     (tree, tag) => exs.foldLeft(tag)((t,ex)=>ex(tree,t))
   }
 
-  lazy val all = List(name, position)
+  lazy val all = List(name, position, kind)
 }
